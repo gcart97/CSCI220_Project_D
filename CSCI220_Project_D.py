779ff0b5bc4602fad70b0636e24d358b75cc664c
gcart97 <gavcart11@gmail.com>
2023-12-04 13:16:48 -0500

    test

--- CSCI220_Project_D.py
+++ CSCI220_Project_D.py
@@ -1,1 +1,13 @@
-print('hello world')
+#CSCI 220 Final Project Group D 
+#12-4-23
+
+#class that holds the attributes for a node
+class HuffmanNode:
+    def __init__(self, char, freq):
+        self.char = char
+        self.freq = freq
+        self.left = None
+        self.right = None
+
+    def __lt__(self, other):
+        return self.freq < other.freq
